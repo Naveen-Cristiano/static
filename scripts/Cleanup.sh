936be61a97f789f6e4d922ee09485bdeaaaad2ed
Mandhadi Naveen Sagar <mandhadi.sagar@ad.infosys.com>
2025-06-13 19:54:52 +0530

    Testing with lfs

--- scripts/Cleanup.sh
+++ scripts/Cleanup.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-rm -rf /var/www/html/*
+version https://git-lfs.github.com/spec/v1
+oid sha256:a04636fe69284144e2dcca7448b9d9103d53de0f265000c982986d54cd87cae1
+size 34
--- scripts/ClearAppspec.sh
+++ scripts/ClearAppspec.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-rm -rf /var/www/html/appspec.yml
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0b33f71b3ca6c7e0404f1979f25dad38f5318cf8f9e064ba41c7c0dc97d402c
+size 44
