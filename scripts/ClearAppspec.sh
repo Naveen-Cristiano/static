31121db3e506edc898889902b56a0ea5b6962388
Mandhadi Naveen Sagar <mandhadi.sagar@ad.infosys.com>
2025-06-13 20:31:05 +0530

    removed lfs

--- scripts/ClearAppspec.sh
+++ scripts/ClearAppspec.sh
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f0b33f71b3ca6c7e0404f1979f25dad38f5318cf8f9e064ba41c7c0dc97d402c
-size 44
+#!/bin/bash
+rm -rf /var/www/html/appspec.yml
+rm -rf /var/www/html/scripts
