6d4a4ada7c4935322f9c79130362e40c2dd22182
Naveen-Cristiano <mandhadisagar3023@gmail.com>
2025-06-24 17:49:38 +0530

    new remove.sh

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf /var/www/html/scripts
-rm -rf /var/www/html/appspec.yml
+[ -d /var/www/html/scripts ] && rm -rf /var/www/html/scripts
+[ -f /var/www/html/appspec.yml ] && rm -rf /var/www/html/appspec.yml
